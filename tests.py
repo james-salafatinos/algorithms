65d62b51c95f459fe8b2535a210f213abdf654dc
james-salafatinos <jamessalafatinos@outlook.com>
2020-09-08 20:14:16 -0500

    initial commit

---
+++ tests.py
@@ -0,0 +1,8 @@
+import numpy as np
+test = np.array([1,5,78,100,121,500,900,1000,11000])
+print(binary_search(test, 11000))
+
+
+import numpy as np
+test = np.array([1,5,78,100,121,500,900,1000,11000])
+print(linear_search(test, 900))
